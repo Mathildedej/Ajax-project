f5372fa9e24f4b33077cf56f6144d88eb5f0faa2
Mathildedej <mathilde.dejaureguiberry@gmail.com>
2020-05-27 16:40:43 +0200

    AJAX, ajout et suppression tache

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,12 +9,16 @@ before_action :authenticate_user!
     @category = Category.find(category_params)
     @task.category = @category
     if @task.save
-      redirect_to root_path
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js { }
+      end
       flash[:notice] = "Task created"
     else
       redirect_to root_path
       flash[:notice] = "Please try again"
     end
+
   end
 
   def edit
@@ -37,7 +41,10 @@ before_action :authenticate_user!
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
-    redirect_to root_path
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
   end
 
 
